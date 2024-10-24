a97bd2a60ffb22d92d3742e4f9670b6b042c8ff7
Cem <cmizci@gmail.com>
2024-10-25 00:44:06 +0300

    changes

--- src/C08_Switchh.java
+++ src/C08_Switchh.java
@@ -11,55 +11,34 @@ public class C08_Switchh {
         //rakam olrak girilen değeri yazı ile yazdırın
 
 
-        String[] birler = new String []{"bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"};
-
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Lütfen bir rakam giriniz..");
         int rakam = scan.nextInt();
 
-        int birlerBasamagı = rakam;
-
-        String yazı = birler[birlerBasamagı - 1];
-
-        System.out.println("yazı = " + yazı);
 
 
-        //kullanıcan gün numarasını alın
-        //sayı olarak girilen günün ismini yazdırın
-        //1- pazartesi ..... 7-pazar
-        //1-7 arası rakamlar dışında bir değer girilirse ,hata mesajı verin
-
-
-        String[] günler = new String []{"Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "C.tesi", "Pazar",};
-
-        Scanner scanner =new Scanner(System.in);
-        System.out.println("Lütfen 1 ile 7 arasında bir rakam giriniz");
-        int sayı = scanner.nextInt();
-
-        int deger = sayı;
-
-        String yazım = günler[deger -1];
-
-        System.out.println(yazım);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (rakam== 1){
+            System.out.println("bir");
+        }else if (rakam==2){
+            System.out.println("iki");
+        }else if (rakam==3){
+            System.out.println("üç");
+        }else if (rakam==4){
+            System.out.println("dört");
+        }else if (rakam==5){
+            System.out.println("beş");
+        }else if (rakam==6){
+            System.out.println("altı");
+        } else if (rakam==7) {
+            System.out.println("yedi");
+        } else if (rakam==8) {
+            System.out.println("sekiz");
+        } else if (rakam == 9) {
+            System.out.println("dokuz");
+        }else {
+            System.out.println("Lütfen 1 ile 9 araında bir sayı giriniz..");
+        }
 
 
     }
